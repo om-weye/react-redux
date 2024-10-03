507d0d7fddfef183b959c2c99d0cb963bad9ac7c
om-weye <omraj.sharma@wheelseye.com>
2024-10-03 22:54:39 +0530

    feat: todos persistance with local storage

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,7 +7,7 @@ const TodoList = () => {
 
   return (
     <ul>
-        {todos.map(todo => <TodoItem todo={todo} />)}
+        {todos.map(todo => <TodoItem key={todo.id} todo={todo} />)}
     </ul>
   )
 }
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,34 @@
 import { createStore } from "redux";
 import todoReducer from "./reducer";
 
+const saveToLocalStorage = (state) => {
+    try {
+        // convert to string
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('todos', serializedState);
+    } catch (e) {
+        console.warn(e)
+    }
+}
+
+const loadFromLocalStorage = () => {
+    try {
+        const serializedState = localStorage.getItem('todos');
+        if (serializedState === null) return undefined;
+        // convert to object
+        return JSON.parse(serializedState)
+    } catch (e) {
+        console.warn(e)
+        return undefined;
+    }
+}
+
 /**
  * STORAGE
  */
-const store = createStore(todoReducer);
+const persistedState = loadFromLocalStorage();
+const store = createStore(todoReducer, persistedState);
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
